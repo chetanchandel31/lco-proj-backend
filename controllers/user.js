83eca64a0e0e6c4e45008c6074aa632f192eac3b
chetan chandel <chetanchandel3@gmail.com>
2022-01-11 20:09:59 +0530

    userPurchaseList and pushOrderInPurchaseList

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,4 @@
+const { Order } = require("../models/order");
 const User = require("../models/user");
 
 exports.getUserById = (req, res, next, id) => {
@@ -30,6 +31,47 @@ exports.updateUser = (req, res) => {
       user.salt = undefined;
       user.encry_password = undefined;
       res.json(user);
+    }
+  );
+};
+
+exports.userPurchaseList = (req, res) => {
+  Order.find({ user: req.profile._id })
+    .populate("user", "_id name") // academind populate explanation
+    .exec((err, order) => {
+      if (err) {
+        return res.status(400).json({ error: "no order in this account" });
+      }
+      return res.json(order);
+    });
+};
+
+exports.pushOrderInPurchaseList = (req, res, next) => {
+  let purchases = [];
+  req.body.order.products.forEach((product) => {
+    purchases.push({
+      _id: product._id,
+      name: product.name,
+      description: product.description,
+      category: product.category,
+      quantity: product.quantity,
+      amount: req.body.order.amount,
+      transaction_id: req.body.order.transaction_id,
+    });
+  });
+
+  // store this in db
+  User.findOneAndUpdate(
+    { _id: req.profile._id },
+    { $push: { purchases: purchases } },
+    { new: true }, // need updated result in callback
+    (err, purchases) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Unable to save purchase list",
+        });
+      }
+      next();
     }
   );
 };
--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,23 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 
-const { getUserById, getUser, updateUser } = require("../controllers/user");
+const {
+  getUserById,
+  getUser,
+  updateUser,
+  userPurchaseList,
+} = require("../controllers/user");
 const { isSignedIn, isAuthenticated } = require("../controllers/auth");
 
-router.param("userId", getUserById);
+router.param("userId", getUserById); // middleware to populate req.profile with User found in db
 
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+router.put(
+  "/user/orders/:userId",
+  isSignedIn,
+  isAuthenticated,
+  userPurchaseList
+);
 
 module.exports = router;
